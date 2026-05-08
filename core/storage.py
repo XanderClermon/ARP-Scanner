f8a5aeddf6f864d152a87314bfdf7050aff7963b
Xander Clairmon <choutte.solaire@proton.me>
2026-05-08 18:10:37 +0400

    Add Redis

--- core/storage.py
+++ core/storage.py
@@ -1,6 +1,9 @@
 import os
 import json
 from core.interfaces import BaseStorage
+import redis
+import time
+from config import settings
 
 class JsonStorage(BaseStorage):
     def __init__(self, filename="devices.json"):
@@ -36,3 +39,28 @@ class JsonStorage(BaseStorage):
             return is_completely_new
 
         return False
+
+
+class RedisStorage(BaseStorage):
+    def __init__(self):
+        self.r = redis.Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            decode_responses=True
+        )
+
+    def add_device(self, ip, mac, **kwargs):
+        # 1. Основная логика твоего хранилища (проверка на новизну и т.д.)
+        # ... твой существующий код ...
+
+        # 2. А теперь пишем в HSET для веб-интерфейса
+        hostname = kwargs.get("hostname", "Unknown")
+        timestamp = time.strftime("%H:%M:%S")
+
+        # Собираем строку: "MAC|Имя|Время"
+        data_string = f"{mac}|{hostname}|{timestamp}"
+
+        # Пишем в Redis
+        self.r.hset("detected_devices", ip, data_string)
+
+        return True  # или результат проверки is_new
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from config import IFACE, TARGET_NETWORK, PAUSE_TIME
-from core.storage import JsonStorage
+from core.storage import JsonStorage, RedisStorage
 from core.daemon import NetDaemon
 from modules.arp_scan import ArpScanner
 from modules.host_resolve import MdnsResolver
 import asyncio
 from modules.os_detect import BasicOSDetector
 
-
 def FactoryDaemon():
     discovery = ArpScanner(target_network=TARGET_NETWORK, interface_name=IFACE)
     enrichers = [MdnsResolver(), BasicOSDetector(tcp_port=80)]
-    storage = JsonStorage()
+    storage = RedisStorage()
     pause = PAUSE_TIME
     return NetDaemon(discovery,storage,enrichers,pause)
 
